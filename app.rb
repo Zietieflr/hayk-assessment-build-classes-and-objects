12471b97b60377d4d203356a687bb3faa8e4f23b
Zietieflr <lrmcguire93@gmail.com>
2020-05-14 18:22:46 -0600

    Removed debugging code

--- app.rb
+++ app.rb
@@ -50,5 +50,3 @@ end
 flatiron = Building.new('Flatiron', '1111 St, Denver Co', 4, 20)
 my_apartment = Building.new('Highpointe Apartments', '2222 Ave, Aurora Co', 3, 60)
 somewhere_else = Building.new('Imagination Station', 'Wingding Lane', 6, 60)
-
-binding.pry
