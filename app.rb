af3109b965e4f579c096f6676031fbe5d5cf3fef
Zietieflr <lrmcguire93@gmail.com>
2020-05-15 12:00:45 -0600

    Edited example object text

--- app.rb
+++ app.rb
@@ -36,5 +36,5 @@ class Building
 end
 
 flatiron = Building.new('Flatiron', '1111 St, Denver Co', 4, 20)
-my_apartment = Building.new('Highpointe Apartments', '2222 Ave, Aurora Co', 3, 60)
+my_apartment = Building.new('These Apartments', '2222 Ave, Aurora Co', 3, 60)
 somewhere_else = Building.new('Imagination Station', 'Wingding Lane', 6, 60)
